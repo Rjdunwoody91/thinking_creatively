406344fcbfe879aedb6e5369227359724237f943
Ryan Dunwoody <rjdunwoody91@gmail.com>
2013-11-05 18:03:00 +0000

    Deleted PHP scripts as don't need them for class

--- myfile.php
+++
@@ -1,5 +0,0 @@
-<?
- $server='mysql9.000webhost.com';
- $user='a2314157_user'
- $pass='Think123';
-?> 
--- process.php
+++
@@ -1,8 +0,0 @@
-<? 
- include 'myfile.php'; 
- $email=$_POST['email']; 
- mysql_connect("server", "user", "pass") or die(mysql_error()); 
- mysql_select_db("a2314157_Leads") or die(mysql_error()); 
- mysql_query("INSERT INTO `data` VALUES ('$email')"); 
- Print "Your information has been successfully added to the database."; 
- ?> 
